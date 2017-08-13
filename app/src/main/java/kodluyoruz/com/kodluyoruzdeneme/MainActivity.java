96cb2369fce1f1bcd6f8652044bb4bf00a212a7e
Feti Can Kirazcı <feticankirazci@gmail.com>
2017-08-13 12:17:44 +0300

    denemecommit

--- app/src/main/java/kodluyoruz/com/kodluyoruzdeneme/MainActivity.java
+++ app/src/main/java/kodluyoruz/com/kodluyoruzdeneme/MainActivity.java
@@ -3,8 +3,11 @@ package kodluyoruz.com.kodluyoruzdeneme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
